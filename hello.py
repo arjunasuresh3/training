458a5204f3037e3e39bdbb7734b2238dba623fac
Arjuna <arjunasuresh3@gmail.com>
2013-04-14 00:37:17 +0530

    added examples

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, jsonify
 app = Flask(__name__)
 
 
@@ -10,5 +10,20 @@ def home():
 def ajaxex():
 	return render_template('ajaxcontent.html')
 
+@app.route('/ajaxxml')
+def ajaxxml():
+    xml = """
+    <root>
+    <name surname='S'>Arjun</name>
+    </root>"""
+    return Response(xml, mimetype='text/xml')        
+
+@app.route('/ajaxjson')
+def ajaxjson():
+    resp = jsonify({'Resultset' :{'Result':{ "Name" : "Arjun",
+                                             "Country" : "India", 
+                                             "favlanguages" : ["javascript", "python", "php"]}}})
+    return resp
+    
 if __name__ == '__main__':
     app.run()
--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,21 +1,75 @@
-function ajax(obj) {
-	console.log(obj);
+function ajaxhtml(obj) {
 	var xmlhttp;
-	if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-	  xmlhttp = new XMLHttpRequest();
+	if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+	      document.getElementById("appendhere").innerHTML=xmlhttp.responseText;
 	  }
-	else
-	  {// code for IE6, IE5
-	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-	xmlhttp.onreadystatechange=function()
-	  {
-	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-		document.getElementById("appendhere").innerHTML=xmlhttp.responseText;
-		}
-	  }
+	};
 	xmlhttp.open("GET","ajaxex",true);
+	xmlhttp.send();
+}
+
+
+function ajaxxml(obj) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+              var xmlstring = xmlhttp.responseText, xmlDoc;
+              if (window.DOMParser) {
+                  var parser = new DOMParser();
+                  xmlDoc = parser.parseFromString(xmlstring,"text/xml");
+              }
+              else{
+                  xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                  xmlDoc.async = false;
+                  xmlDoc.loadXML(xmlstring);
+              }
+              var name = xmlDoc.documentElement.getElementsByTagName('name')[0],
+              surname = name.getAttribute('surname'),
+              myName = name.firstChild.nodeValue;
+	      document.getElementById("xmlcontent").innerHTML = "My name is " + myName + " " + surname + "!!!";
+	  }
+	};
+	xmlhttp.open("GET","ajaxxml",true);
+	xmlhttp.send();
+}
+
+
+function ajaxjson(obj) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+              var jsonObj = JSON.parse(xmlhttp.responseText),
+              h1Obj = document.createElement("h1"),
+              h2Obj = document.createElement("h2"),
+              h3Obj = document.createElement("h3"),
+	      jsonAppend = document.getElementById("jsoncontent");
+              h1Obj.innerHTML = jsonObj.Resultset.Result.Name;
+              h2Obj.innerHTML = jsonObj.Resultset.Result.Country;
+              h3Obj.innerHTML = jsonObj.Resultset.Result.favlanguages;
+              jsonAppend.appendChild(h1Obj);
+              jsonAppend.appendChild(h2Obj);
+              jsonAppend.appendChild(h3Obj);
+	  }
+	};
+	xmlhttp.open("GET","ajaxjson",true);
 	xmlhttp.send();
 }
